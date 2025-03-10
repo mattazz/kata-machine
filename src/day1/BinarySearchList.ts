85b58f9215da25b960b2828a94947f87c1635669
Mattazz <matthewazada101@gmail.com>
2025-03-09 21:57:16 -0400

    feat: implement binary search, linear search, and two crystal balls algorithms

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,16 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
-
+    let lo = 0; // lowest of array
+    let hi = haystack.length; // top most of array
+    do {
+        const m = Math.floor(lo + (hi - lo) / 2); // getting the midpoint INDEX
+        const v = haystack[m]; // setting the value based on mid index
+        if (v === needle) {
+            return true;
+        } else if (v > needle) {
+            hi = m;
+        } else {
+            lo = m + 1;
+        }
+    } while (lo < hi);
+    return false;
 }
