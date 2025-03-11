1aec04d5ff284dcdc1a363580340a2cc86f291ad
Mattazz <matthewazada101@gmail.com>
2025-03-11 13:48:34 -0400

    feat: implement bubble sort algorithm and enhance queue class with enqueue and deque methods

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,12 @@
 export default function bubble_sort(arr: number[]): void {
+    for (let i = 0; i < arr.length; ++i) {
+        for (let j = 0; j < arr.length - 1 - i; ++j) {
+            if (arr[j] > arr[j + 1]) { // comparison
+                const tmp = arr[j]; // create swap temp var
+                arr[j] = arr[j + 1]; // moves arr[j+1] into arr[j]
+                arr[j + 1] = tmp; // moves arr[j] into arr[j+1]
+            }
+        }
+    }
 
-}
+} 
--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,38 @@
+type Node<T> = {
+    value: T,
+    next?: Node<T>,
+    prev?: Node<T>
+}
+
 export default class Queue<T> {
     public length: number;
-
-    
+    private head?: Node<T>; // refers to the node 
+    private tail?: Node<T>; // refers to the node
 
     constructor() {
+        this.head = this.tail = undefined;
+         this.length = 0;
+        
     }
 
-    enqueue(item: T): void {
+    enqueue(item: T): void { // Add to tail
 
 }
-    deque(): T | undefined {
+    deque(): T | undefined { // remove from head
+        if (!this.head){
+            return undefined;
+        }
+        this.length--;
+        
+        const head = this.head; // save your head
+        this.head = this.head.next; // update the head
 
+        // Free
+        head.next = undefined
+
+        return head.value;
 }
-    peek(): T | undefined {
-
+    peek(): T | undefined { // peak at head.val
+        return this.head?.value;
 }
 }
